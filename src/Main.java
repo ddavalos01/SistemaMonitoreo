2fa0b62745506b64fc1f22207816231df8da559e
ddavalos01 <ddavalos01@alumnos.uaq.mx>
2026-09-17 10:21:29 -0600

    Se borraron las modificaciones hechas al main que debieron haber sido hechas en una branch

--- src/Main.java
+++ src/Main.java
@@ -1,29 +1,5 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Sistema de monitoreo\n");
-
-        Tanque yeet = new Tanque("ayy1", 515.0);
-
-        yeet.mostrarResumen();
     }
 }
-/*
-enum TankState{
-    LLENANDO, VACIANDO, DETENIDO
-}
-
-public class Main{
-    public static final int LLENADO=1;
-
-    public static void main(String[] args){
-
-        System.out.println(LLENANDO);
-
-        TankState estado=TankState.DETENIDO;
-
-        System.out.println(estado);
-
-    }
-
-}
- */
